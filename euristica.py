28b285caf40d937c58061ef8456df71f40491919
Luigi Lotito <luigi.lotito@student.unife.it>
2021-06-26 15:30:01 +0200

    removed input.xlsx

---
+++ euristica.py
@@ -0,0 +1,7 @@
+import input
+import batch
+
+batches = []
+
+
+
--- input.py
+++ input.py
@@ -3,6 +3,7 @@ from numpy import array
 from configuration import INPUT_FILE
 
 file_excel = load_workbook(INPUT_FILE, data_only=True)
+print(file_excel.sheetnames)
 # dal primo foglio della mia cartella excel
 sheet1 = file_excel["Sheet1"]
 n = sheet1['B1'].value # vado a leggere il numero di jobs
