91c62ff15c5ae32ba6d895a4c8f68cea76d24b09
Luigi Lotito <luigi.lotito@student.unife.it>
2021-07-02 16:33:28 +0200

    modifiche classe batch

--- model/batch.py
+++ model/batch.py
@@ -11,27 +11,24 @@ class Batch:
 
     def get_max_dur(self):
         dur_max = 0
-        for i in range(len(self.j_t)):
+        for i in self.j_t:
             if dur_max < self.j_t[i][1].duration:
                 dur_max = self.j_t[i][1].duration
         print(dur_max)
         return dur_max
 
     def empty_batch(self):
-        if len(self.j_t) == 0:
-            return True
-        return False
+        return self.j_t == [[]] or not self.j_t
 
     def full_batch(self):
-        if len(self.j_t) == self.capacity:
-            return True
-        return False
+        return len(self.j_t) == self.capacity
 
     def is_job_in(self, job_id):
-        for i in range(len(self.j_t) - 1):
-            if self.j_t[i][0].j_id == job_id:
-                return True
-        return False
+        # for i in range(len(self.j_t) - 1):
+        #     if self.j_t[i][0].j_id == job_id:
+        #         return True
+        # return False
+        return job_id in [x[0].id for x in self.j_t]
 
     def add_task(self, job, task):
         if not self.full_batch():
@@ -39,7 +36,7 @@ class Batch:
 
     def remove_task(self, job, task):
         if not self.empty_batch():
-            for i in range(len(self.j_t)):
+            for i in self.j_t:
                 if self.j_t[i][0] == job and self.j_t[i][1] == task:
                     del self.j_t[i]
 
