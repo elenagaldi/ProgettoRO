3983c003ff9284705bcded0d0b4df9c90881e850
Andrei <andreibaciu87@yahoo.it>
2021-07-07 21:34:30 +0100

    Ordinamento dei task, manca l'algoritmo di esecuzione

--- euristica.py
+++ euristica.py
@@ -14,31 +14,60 @@ class Greedy:
 
     def labeling(self):
         self.jobs.sort(key=lambda x: x.release_time)
-        num_jobs = len(self.jobs)
-        job_i = 0
+
         task_i = 0
         j_t = []
         batches: [Batch] = []
-
-        job = self.jobs[job_i]
-        start_next_batch = job.release_time
-
         tasks_processed = 0
         id_batch = 0
-        while tasks_processed < self.tot_task:
+        self.jobs.sort(key=lambda x: x.due_date)
+        length = len(self.jobs)
+        middle_index = length // 2
+        prima_meta = self.jobs[:middle_index]
+        seconda_meta = self.jobs[middle_index:]
+        prima_meta.sort(key=lambda x: x.release_time)
+        seconda_meta.sort(key=lambda x: x.release_time)
+        print(prima_meta)
+        print('Le differenze\n')
+        print(seconda_meta)
+        z = len(prima_meta)
+        y = len(seconda_meta)
+        num_jobs = z
+        # Qui conto quanti sono i task dentro ogni lista
+        primi_task = []
+        print(prima_meta)
+
+        for i in prima_meta:
+            for t in i.task:
+                primi_task.append([i.id, t.duration])
+
+        primi_task.sort(key=lambda x: x[1])
+        print(primi_task)
+
+        secondi_task = []
+
+        for i in seconda_meta:
+            for t in i.task:
+                secondi_task.append([i.id, t.duration])
+        secondi_task.sort(key=lambda x: x[1])
+        print(secondi_task)
+
+        job = self.jobs[primi_task[task_i][0]]
+        start_next_batch = job.release_time
+
+        while tasks_processed < len(primi_task):
             k = 0
-            while k in range(self.m) and job.release_time <= start_next_batch and job_i < num_jobs:
-                if not job.task[task_i].is_processed():
-                    j_t.append([job.id, job.task[task_i]])
-                    job.task[task_i].set_processed(True)
-                    tasks_processed += 1
-                    task_i += 1
-                    if self.jobs[job_i].is_completed():
-                        job.last_batch = id_batch
-                        job_i += 1
-                        task_i = 0
-                        job = self.jobs[job_i] if job_i < num_jobs else job
-                    k += 1
+            while k in range(self.m) and self.jobs[primi_task[task_i][0]].release_time <= start_next_batch and tasks_processed < len(primi_task):
+                for t in self.jobs[primi_task[task_i][0]].task:
+                    print(t.duration, primi_task[task_i][0], primi_task[task_i][1])
+                    if t.duration == primi_task[task_i][1] and not t.is_processed:
+                        # if not t.is_processed():
+                        j_t.append([primi_task[task_i][0], t])
+                        t.set_processed(True)
+                        if self.jobs[primi_task[task_i][0]].is_completed():
+                            self.jobs[primi_task[task_i][0]].last_batch = id_batch
+                        k += 1
+                        task_i += 1
             batch = Batch(id_batch, self.m, j_t, start_next_batch)
             batches.append(batch)
             start_next_batch = max(batch.end, job.release_time)
@@ -46,4 +75,24 @@ class Greedy:
             id_batch += 1
 
         print(f'Batches : {batches}')
+
+        # Qui conto quanti sono i task dentro ogni lista
+        primi_task = []
+        print(prima_meta)
+
+        for i in prima_meta:
+            for t in i.task:
+                primi_task.append([i.id, t.duration])
+
+        primi_task.sort(key=lambda x: x[1])
+        print(primi_task)
+
+        secondi_task = []
+
+        for i in seconda_meta:
+            for t in i.task:
+                secondi_task.append([i.id, t.duration])
+        secondi_task.sort(key=lambda x: x[1])
+        print(secondi_task)
+
         return batches
