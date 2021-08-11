4bc0b2374c861b204be7ef5b4066008249dc37a1
Luigi Lotito <luigi.lotito@student.unife.it>
2021-08-11 12:27:02 +0200

    implementata ILS e collegata alla simulated annealing

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from firstSolution.greedy2 import Greedy2
 from firstSolution.greedy3 import Greedy3
 from inputData.xlsInputData import XslInputData
 from model.solution import Solution
-from optimization import localSearch
+from optimization import localSearch, iteratedLS
 from optimization.acceptanceCriteria import *
 
 
@@ -17,7 +17,7 @@ def list_to_dict(ll: list, count=None):
 
 if __name__ == '__main__':
 
-    input_obj = XslInputData(configuration.INPUT_FILE)
+    input_obj = XslInputData(configuration.INPUT_FILE4)
     jobs, jobs_num, task_num, capacity_batch, durate_task_l = input_obj.read_jobs()
 
     jobs_dict: dict = list_to_dict(jobs, jobs_num)
@@ -27,22 +27,46 @@ if __name__ == '__main__':
     for i in jobs:
         tot_task += len(i.task)
 
-    greedy = Greedy2(jobs, capacity_batch, tot_task)
+    greedy = Greedy3(jobs, capacity_batch, tot_task)
     batches = greedy.start()
 
     initial_solution = Solution(batches, jobs_dict)
     initial_cost = initial_solution.obj_function(count_vincoli=False)
     print(f'Costo:\n {initial_cost}')
 
-    solution, cost, numero_ricerche = local_search(initial_solution, best_improvement_strategy=True)
+    solution, cost = iteratedLS.start(initial_solution)
+    print(solution.batches, f'Ottimo trovato con ILS: {cost} \n')
 
-    print(solution.batches,
-          f'Ottimo locale trovato con ricerca locale : {cost} \n Numero scambi: {numero_ricerche - 1}')
+    # solution, cost, n = local_search(initial_solution, best_improvement_strategy=True)
+    #
+    # print(solution.batches,
+    #       f'Ottimo locale trovato con ricerca locale : {cost} \n Numero scambi:{n}')
 
-    solution.analyze_delay()
+    # SA_solution, SA_cost, = simulated_annealing(solution)
+    # print(SA_solution.batches, f'Ottimo locale trovato con simulated annealing : {SA_cost}')
 
-    cost, solution = simulated_annealing(solution)
-    print(solution.batches, f'Ottimo locale trovato con simulated annealing : {cost}')
+    # solution, cost, numero_ricerche = local_search(SA_solution, best_improvement_strategy=True)
+    #
+    # print(solution.batches,
+    #       f'Ottimo locale trovato con ricerca locale : {cost} \n Numero scambi: {numero_ricerche - 1}')
+    # l = []
+    # c = []
+    # for i in range(10):
+    #     SA_cost, SA_solution = simulated_annealing(solution)
+    #     print(f'Ottimo locale trovato con simulated annealing : {SA_cost}')
+    #     if SA_cost not in l:
+    #         l.append(SA_cost)
+    #         c.append(1)
+    #     else:
+    #         i = l.index(SA_cost)
+    #         c.insert(i, c[i] + 1)
+    #
+    # lc = list(zip(l, c))
+    # lc.sort(key=lambda xx: xx[1], reverse=True)
+    # for y in lc:
+    #     print(f'Costo: {y[0]}, occorrenze: {y[1]}')
+
+    # solution.analyze_delay()
 
     '''cost = 0
     cost, batches = destroy_repair\
--- optimization/acceptanceCriteria.py
+++ optimization/acceptanceCriteria.py
@@ -6,26 +6,31 @@ from model.solution import Solution
 from optimization.localSearch import *
 from random import *
 
-TEMP_EQ = 5
-
 
 def simulated_annealing(solution: Solution):
+    TEMP_EQ = 10
     current_solution = copy.deepcopy(solution)
     current_cost = solution.obj_function(count_vincoli=True)
     best_solution, best_cost = solution, current_cost
     t = 0.2 * current_cost
 
-    while t > 0.1:
+    while t > 0.0001:
         for k in range(TEMP_EQ):  # imposto il ciclo fino al raggiungimento dell'equilibrio
             next_solution = copy.deepcopy(current_solution)
-            batch1 = choice(solution.batches)
+
+            temp = copy.deepcopy(next_solution)
+
+            batch1 = choice(temp.batches)
             pos1 = batch1.id
-            batch2 = choice(solution.batches)
+            temp.batches.remove(batch1)
+            batch2 = choice(temp.batches)
             pos2 = batch2.id
+            del temp
+
             next_solution.swap_batches(batch1, batch2, pos1, pos2)
             next_cost = next_solution.obj_function(count_vincoli=True)
 
-            # next_cost, next_solution = search_with_worsening(current_solution)
+            # next_cost, next_solution = search_with_worsening(next_solution)
 
             deltaE = next_cost - current_cost
             if deltaE < 0:  # è stato ottenuto un miglioramento quindi accetto la soluzione trovata
@@ -38,6 +43,47 @@ def simulated_annealing(solution: Solution):
                 r = random()
                 if r < math.exp(-(deltaE / t)):
                     current_solution = copy.deepcopy(next_solution)
+                    current_cost = next_cost
         t = t / 2  # abbasso la temperatura
 
-    return best_cost, best_solution
+    return best_solution, best_cost
+
+
+class SimulatedAnnealing:
+    def __init__(self, initial_solution: Solution, temp_eq=2, max_t=1):
+        self.TEMP_EQ = temp_eq
+        self.MAX_T = max_t
+        self.current_solution = copy.deepcopy(initial_solution)
+        self.current_cost = initial_solution.obj_function(count_vincoli=True)
+        self.best_solution, self.best_cost = initial_solution, self.current_cost
+        self.t = 0.2 * self.current_cost
+        self.k = 0
+
+    def next_step(self):
+        self.k += 1
+        if self.k >= self.TEMP_EQ:
+            self.k = 0
+            self.t = self.t / 2
+
+    def stop_condition(self):
+        return self.t <= self.MAX_T
+
+    def acceptance_test(self, next_solution, next_cost):
+        deltaE = next_cost - self.current_cost
+        if deltaE < 0:
+            self.current_solution = next_solution
+            self.current_cost = next_cost
+            if next_cost < self.best_cost:
+                self.best_solution = next_solution
+                self.best_cost = next_cost
+        else:
+            r = random()
+            if r < math.exp(-(deltaE / self.t)):
+                self.current_solution = next_solution
+                self.current_cost = next_cost
+        self.k += 1
+        if self.k >= self.TEMP_EQ:
+            self.k = 0
+            self.t = self.t / 2
+
+        return self.current_solution, self.current_cost
